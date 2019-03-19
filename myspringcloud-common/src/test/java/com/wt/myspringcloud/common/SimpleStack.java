eea5be1960e8716d0a031685ddce5b1b50edab37
文通 <340192693@qq.com>
2019-03-19 22:40:03 +0800

    栈的实现

---
+++ myspringcloud-common/src/test/java/com/wt/myspringcloud/common/SimpleStack.java
@@ -0,0 +1,27 @@
+package com.wt.myspringcloud.common;
+
+/**
+ * 简单栈
+ */
+public class SimpleStack {
+
+    private int[] stack; //数组
+    private int stackPointer = 0;
+
+    public void push(int i) {
+        stack[stackPointer] = i;
+        stackPointer++;
+    }
+
+    public int read() {
+        stackPointer--;
+        if (stackPointer >= 0) {
+            return stack[stackPointer];
+        } else {
+            stackPointer = 0;
+            return 0;
+        }
+        
+    }
+
+}
