e3473cf8c0e386c8da7d202e5b014e6e0303f904
文通 <340192693@qq.com>
2020-04-28 21:46:34 +0800

    调整：调整部分注解和依赖

--- myspringcloud-common/src/main/java/com/wt/myspringcloud/common/pojo/req/UserReq.java
+++ myspringcloud-common/src/main/java/com/wt/myspringcloud/common/pojo/req/UserReq.java
@@ -1,12 +1,17 @@
 package com.wt.myspringcloud.common.pojo.req;
 
 import com.wt.myspringcloud.common.core.BaseReq;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
 import java.util.StringJoiner;
 
 public class UserReq extends BaseReq {
 
     private Long id;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dateTime;
 
     @Override
     public String toString() {
