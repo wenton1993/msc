07db4dbc11cf225b5985c1368b8b5b95ca4f66b5
文通 <340192693@qq.com>
2020-12-14 16:10:11 +0800

    build: 调整项目层次

---
+++ msc-common-new/msc-common-feign/src/main/com/wt/msc/common/feign/component/feign/UserFeign.java
@@ -0,0 +1,10 @@
+package com.wt.msc.common.feign.component.feign;
+
+import com.wt.myspringcloud.common.api.user.UserApi;
+
+/**
+ * @author 文通
+ * @since 2020/12/14
+ */
+public interface UserFeign extends UserApi {
+}
